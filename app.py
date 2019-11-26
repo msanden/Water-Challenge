cf0325792e943d7540d81e3eea88baefb7e26c9f
Manuel Sande <mnlsande22@gmail.com>
2019-11-26 17:49:56 -0500

    add title to tab

--- app.py
+++ app.py
@@ -2,5 +2,6 @@ import dash
 import dash_bootstrap_components as dbc
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.title = 'IBM Water Challenge'
 server = app.server
 app.config.suppress_callback_exceptions = True
