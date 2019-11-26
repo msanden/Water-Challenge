9d36e5f8896cdbe13e3f26b6e3f9b219f9e22e4e
Manuel Sande <mnlsande22@gmail.com>
2019-11-26 17:04:52 -0500

    Add sidebar in entry point file

--- index.py
+++ index.py
@@ -1,25 +1,84 @@
+
+import dash
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
 from app import app
-from layouts import layout1, layout2
-import callbacks
+from apps import app1, app2
 
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
-])
 
-@app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/apps/app1':
-         return layout1
-    elif pathname == '/apps/app2':
-         return layout2
-    else:
-        return '404'
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
 
-if __name__ == '__main__':
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar = html.Div(
+    [
+        html.H2("IBM Water Challenge", className="display-6"),
+
+        html.Hr(),
+
+        html.P("Waterpoint Services at County Level"),
+
+        dbc.Nav([
+                dbc.NavLink("Overview", href="/page-1", id="page-1-link"),
+                dbc.NavLink("Waterpoints", href="/page-2", id="page-2-link"),
+                ],
+                vertical=True, pills=True
+                ),
+    ],
+    style=SIDEBAR_STYLE)
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
+
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+
+
+# this callback uses the current pathname to set the active state of the
+# corresponding nav link to true, allowing users to tell page they are on
+@app.callback(
+    [Output(f"page-{i}-link", "active") for i in range(1, 3)],
+    [Input("url", "pathname")])
+def toggle_active_links(pathname):
+    if pathname == "/":
+        # Treat page 1 as the homepage / index
+        return True, False
+    return [pathname == f"/page-{i}" for i in range(1, 3)]
+
+
+@app.callback(
+    Output("page-content", "children"),
+    [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname in ["/", "/page-1"]:
+        return app1.layout
+    elif pathname == "/page-2":
+        return app2.layout
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
+
+
+if __name__ == "__main__":
     app.run_server(debug=True)
