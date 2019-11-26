4ab2225373ed35ac833660f314895f53d7b21fa2
Manuel Sande <mnlsande22@gmail.com>
2019-11-26 17:49:38 -0500

    retrieve data and replace starter code

--- apps/app1.py
+++ apps/app1.py
@@ -4,22 +4,56 @@ from dash.dependencies import Input, Output
 
 from app import app
 
+
+url = 'https://waterpoint-engine-challenge-dev.mybluemix.net/sensors/daily-county-readings/{}'
+counties = ['Wajir','Turkana','Garissa','Marsabit','Isiolo']
+waterpoint_fields = ['county','siteName','expertStatus','siteLon','siteLat','localDate','mlStatusPred']
+
+
+
 layout = html.Div([
-    html.H3('App 1'),
+    'Choose a city:',
     dcc.Dropdown(
-        id='app-1-dropdown',
-        options=[
-            {'label': 'App 1 - {}'.format(i), 'value': i} for i in [
-                'NYC', 'MTL', 'LA'
-            ]
-        ]
+        id='persisted-city',
+        value='Montreal',
+        options=[{'label': v, 'value': v} for v in CITIES],
+        persistence=True
     ),
-    html.Div(id='app-1-display-value'),
+
+    html.Br(),
+
+    'correlated persistence - choose a neighborhood:',
+    html.Div(
+        dcc.Dropdown(
+            id='neighborhood'
+            
+            ), id='neighborhood-container'),
+    
+    html.Br(),
+    
+    html.Div(id='persisted-choices')
 ])
 
 
 @app.callback(
-    Output('app-1-display-value', 'children'),
-    [Input('app-1-dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+    Output('neighborhood-container', 'children'),
+    [Input('persisted-city', 'value')]
+)
+def set_neighborhood(city):
+    neighborhoods = NEIGHBORHOODS[city]
+    return dcc.Dropdown(
+        id='neighborhood',
+        value=neighborhoods[0],
+        options=[{'label': v, 'value': v} for v in neighborhoods],
+        persistence_type='local',
+        persistence=city
+    )
+
+
+@app.callback(
+    Output('persisted-choices', 'children'),
+    [Input('persisted-city', 'value'), Input('neighborhood', 'value')]
+)
+def set_out(city, neighborhood):
+    return 'You chose: {}, {}'.format(neighborhood, city)
+
