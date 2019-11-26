3f1db6de321ceee444e3c6887ab4d0e25230e6c8
Manuel Sande <mnlsande22@gmail.com>
2019-11-26 15:54:13 -0500

    update callback logic to switch between pages

--- callbacks.py
+++ callbacks.py
@@ -3,13 +3,28 @@ from dash.dependencies import Input, Output
 from app import app
 
 @app.callback(
-    Output('app-1-display-value', 'children'),
-    [Input('app-1-dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+    [Output(f"page-{i}-link", "active") for i in range(1, 3)],
+    [Input("url", "pathname")])
+def toggle_active_links(pathname):
+    if pathname == "/":
+        # Treat page 1 as the homepage / index
+        return True, False, False
+    return [pathname == f"/page-{i}" for i in range(1, 3)]
+
 
 @app.callback(
-    Output('app-2-display-value', 'children'),
-    [Input('app-2-dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+    Output("page-content", "children"),
+    [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname in ["/", "/page-1"]:
+        return layout1
+    elif pathname == "/page-2":
+        return layout2
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
