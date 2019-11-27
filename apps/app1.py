7b66ffce9eedb62e15ff764fe771ce209d4ddb8c
Manuel Sande <mnlsande22@gmail.com>
2019-11-27 02:01:15 -0500

    add data, style table, remove  dropdown

--- apps/app1.py
+++ apps/app1.py
@@ -1,59 +1,75 @@
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
+
+import requests, json
+import pandas as pd
+from pandas import DataFrame
+import dash_table
 
 from app import app
 
 
 url = 'https://waterpoint-engine-challenge-dev.mybluemix.net/sensors/daily-county-readings/{}'
 counties = ['Wajir','Turkana','Garissa','Marsabit','Isiolo']
-waterpoint_fields = ['county','siteName','expertStatus','siteLon','siteLat','localDate','mlStatusPred']
+waterpoint_fields = ['mWaterId','county','siteName','expertStatus','siteLon','siteLat','localDate','mlStatusPred']
 
+waterpoint_data = []
+for county in counties:
+    j_resp = requests.get(url.format(county)).json()
+
+    waterpoint_sites = {
+                'data': j_resp['data']
+                }
+    waterpoint_data.append(waterpoint_sites)
+
+empty_df = []
+for index in range(len(waterpoint_data)):
+    for key in waterpoint_data[index]:
+        chopped_df = pd.DataFrame(waterpoint_data[index][key])
+    empty_df.append(chopped_df)
+
+df = pd.concat(empty_df)
+county_data = DataFrame(df, columns=waterpoint_fields)
+
+PAGE_SIZE = 50
 
 
 layout = html.Div([
-    'Choose a city:',
-    dcc.Dropdown(
-        id='persisted-city',
-        value='Montreal',
-        options=[{'label': v, 'value': v} for v in CITIES],
-        persistence=True
-    ),
 
-    html.Br(),
+# store = dcc.Store(id='my-store', data={'my-data': 'data'})
 
-    'correlated persistence - choose a neighborhood:',
-    html.Div(
-        dcc.Dropdown(
-            id='neighborhood'
+    html.Div([
+        dash_table.DataTable(
+            id='datatable',
+            data=county_data.to_dict('records'),
+            columns=[{"name": i, "id": i} for i in county_data.columns],
+
+            page_action='native',
+            page_size = PAGE_SIZE,
+
+            sort_action='native',
+            selected_rows=[],
             
-            ), id='neighborhood-container'),
-    
-    html.Br(),
-    
-    html.Div(id='persisted-choices')
-])
+            style_header={'backgroundColor': 'white','fontWeight': 'bold'},
+            style_as_list_view=True,
+            style_cell={'textAlign': 'left'},
+            # style_cell_conditional=[
+            #     {
+            #         'if': {'column_id': i},
+            #         'textAlign': 'left'
+            #     } for c in ['Date', 'Region']
+            # ],
+
+            )]
+        ),
+
+    ])
 
 
-@app.callback(
-    Output('neighborhood-container', 'children'),
-    [Input('persisted-city', 'value')]
-)
-def set_neighborhood(city):
-    neighborhoods = NEIGHBORHOODS[city]
-    return dcc.Dropdown(
-        id='neighborhood',
-        value=neighborhoods[0],
-        options=[{'label': v, 'value': v} for v in neighborhoods],
-        persistence_type='local',
-        persistence=city
-    )
 
 
-@app.callback(
-    Output('persisted-choices', 'children'),
-    [Input('persisted-city', 'value'), Input('neighborhood', 'value')]
-)
-def set_out(city, neighborhood):
-    return 'You chose: {}, {}'.format(neighborhood, city)
+
+
 
