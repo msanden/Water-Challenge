6f458b2b598e8e52e6b8854dbdb5fa48517a4db6
Manuel Sande <mnlsande22@gmail.com>
2019-11-27 07:35:40 -0500

    create make_request/table_data functions, style datatable

--- apps/app2.py
+++ apps/app2.py
@@ -37,24 +37,29 @@ url = 'https://waterpoint-engine-challenge-dev.mybluemix.net/sensors/daily-count
 counties = ['Wajir','Turkana','Garissa','Marsabit','Isiolo']
 waterpoint_fields = ['mWaterId','county','siteName','expertStatus','households','siteLon','siteLat','mlStatusPred']
 
-waterpoint_data = []
-for county in counties:
-    j_resp = requests.get(url.format(county)).json()
+def make_request(url):
+    waterpoint_data = []
+    for county in counties:
+        j_resp = requests.get(url.format(county)).json()
+        waterpoint_sites = {'data': j_resp['data']}
+        waterpoint_data.append(waterpoint_sites) 
+    return waterpoint_data
 
-    waterpoint_sites = {
-                'data': j_resp['data']
-                }
-    waterpoint_data.append(waterpoint_sites)
+waterpoint_data = make_request(url)
 
-empty_df = []
-for index in range(len(waterpoint_data)):
-    for key in waterpoint_data[index]:
-        chopped_df = pd.DataFrame(waterpoint_data[index][key])
-    empty_df.append(chopped_df)
+def table_data(list_dict):
+    empty_df = []
+    for index in range(len(list_dict)):
+        for key in list_dict[index]:
+            broken_df = pd.DataFrame(list_dict[index][key])
+        empty_df.append(broken_df)
+    df = pd.concat(empty_df)
+    return df
 
-df = pd.concat(empty_df)
+df = table_data(waterpoint_data)
 
 county_data = DataFrame(df, columns=waterpoint_fields)
+
 PAGE_SIZE = 50
 
 layout = html.Div([
@@ -70,18 +75,28 @@ layout = html.Div([
                 )
         ]),
 
+        html.Br(),
+
         html.Div([
             dash_table.DataTable(
                 id='datatable',
                 data=county_data.to_dict('records'),
-                columns=[{"name": i, "id": i, "deletable": True} for i in sorted(county_data.columns)],
+                columns=[{"name": i, "id": i, "deletable": True} for i in county_data.columns],
 
                 page_action='native',
                 page_size = PAGE_SIZE,
 
                 sort_action='native',
-                selected_rows=[])
+                selected_rows=[],
+                style_table = {'overflowX': 'scroll','overflowY': 'scroll', 'border': 'thin lightgrey solid'},            
+                style_header={'backgroundColor': 'white','fontWeight': 'bold'},
+                style_as_list_view=True,
+                style_cell={'textAlign': 'left'},
+                ),
+
         ]),
+
+        html.Br(), html.Br(),
 
         html.Div([dcc.Graph(id='bar-graph')]),
 
