aafe1232e1bd26a4f1c66461123a3c55c91753b9
Manuel Sande <mnlsande22@gmail.com>
2019-11-28 14:19:23 -0500

    deploy to heroku

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import dash
 import dash_bootstrap_components as dbc
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-# app.config.from_object(os.environ['APP_SETTINGS'])
 
 app.title = 'IBM Water Challenge'
 server = app.server
