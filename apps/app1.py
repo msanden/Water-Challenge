3637a55bd4417cc02a13e78b2cd7f9946e50555d
Manuel Sande <mnlsande22@gmail.com>
2019-11-28 17:11:57 -0500

    Refactor Code

--- apps/app1.py
+++ apps/app1.py
@@ -11,37 +11,18 @@ from pandas import DataFrame
 import dash_table
 
 from app import app
+from .resource import make_request, table_data
 
 
 requests_cache.install_cache('waterpoint_cache', backend='sqlite', expire_after=86400)
 url = 'https://waterpoint-engine-challenge-dev.mybluemix.net/sensors/daily-county-readings/{}'
-counties = ['Garissa','Isiolo','Marsabit','Turkana','Wajir']
-def make_request(url):
-    waterpoint_data = []
-    for county in counties:
-        j_resp = requests.get(url.format(county)).json()
-        waterpoint_sites = {'data': j_resp['data']}
-        waterpoint_data.append(waterpoint_sites) 
-    return waterpoint_data
-
 waterpoint_data = make_request(url)
-
-def table_data(list_dict):
-    empty_df = []
-    for index in range(len(list_dict)):
-        for key in list_dict[index]:
-            broken_df = pd.DataFrame(list_dict[index][key])
-        empty_df.append(broken_df)
-    df = pd.concat(empty_df)
-    return df
-
 df = table_data(waterpoint_data)
 waterpoint_fields = ['mWaterId','county','siteName','expertStatus','households','siteLon','siteLat','mlStatusPred']
 df = DataFrame(df, columns=waterpoint_fields)
 df = df.drop_duplicates()
 
 mapbox_access_token = 'pk.eyJ1IjoibWFudWVsc24iLCJhIjoiY2szY2k5dTF2MHNhejNjbGRyNnNzcmF4dCJ9.-bX5MXB3Ezlbnwsr-JA1kA'
-
 
 layout = html.Div([
     
---
+++ apps/resource.py
@@ -0,0 +1,25 @@
+
+import requests, requests_cache, json
+import pandas as pd
+from pandas import DataFrame
+
+counties = ['Garissa','Isiolo','Marsabit','Turkana','Wajir']
+def make_request(url):
+    waterpoint_data = []
+    for county in counties:
+        j_resp = requests.get(url.format(county)).json()
+        waterpoint_sites = {'data': j_resp['data']}
+        waterpoint_data.append(waterpoint_sites) 
+    return waterpoint_data
+
+
+def table_data(list_dict):
+    empty_df = []
+    for index in range(len(list_dict)):
+        for key in list_dict[index]:
+            broken_df = pd.DataFrame(list_dict[index][key])
+        empty_df.append(broken_df)
+    df = pd.concat(empty_df)
+    return df
+
+
