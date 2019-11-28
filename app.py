4b2e95e78ce2adff44124669b58ad38208bf3158
Manuel Sande <mnlsande22@gmail.com>
2019-11-28 11:29:05 -0500

    Add app setting configuration for deployment

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+import os
 import dash
 import dash_bootstrap_components as dbc
 
 app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+app.config.from_object(os.environ['APP_SETTINGS'])
+
 app.title = 'IBM Water Challenge'
 server = app.server
 app.config.suppress_callback_exceptions = True
